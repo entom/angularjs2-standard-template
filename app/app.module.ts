1d8080f91cc9499a313fdfcbcfc5d8edd17a54a5
tomasz <tomasznicieja@gmail.com>
2016-08-14 20:14:57 +0200

    basic structure of module/component in angular 2 application

---
+++ app/app.module.ts
@@ -0,0 +1,15 @@
+/**
+ * Created by Nicieja Tomasz on 14.08.2016.
+ */
+
+import { NgModule }      from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent }  from './app.component';
+
+@NgModule({
+    imports:      [ BrowserModule ],
+    declarations: [ AppComponent ],
+    bootstrap:    [ AppComponent ]
+})
+export class AppModule { }
